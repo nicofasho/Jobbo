5aa6818691ae98c959105724b2b72162b2b65813
Christian Watson <chris23w@gmail.com>
2020-05-04 11:19:05 -0700

    slight scheduler change

--- app/scheduler.py
+++ app/scheduler.py
@@ -3,13 +3,13 @@ from datetime import datetime
 from apscheduler.schedulers.background import BackgroundScheduler
 from .models import Scrape, Job
 
-print('starting scheduler...')
 
 def new_scrape():
     s = Scrape()
     s.save()
 
 def start_background_job():
+    print('starting scheduler...')
     scheduler = BackgroundScheduler()
     scheduler.add_job(new_scrape, 'cron', day_of_week='mon', hour=9, minute=0)
     scheduler.start()
